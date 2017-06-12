b37eb0682ff5554b3436c31718e5a7a6c010d344
Shafhira Briggita <bshafhira28@gmail.com>
2017-06-12 12:57:36 +0700

    project selesai less mainactivity

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl431/tugaspribadi/Source.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl431.tugaspribadi;
+
+import java.util.List;
+
+public class Source {
+    public String id;
+    //public String name;
+    //public String description;
+    public String title;
+    public String overview;
+    public String poster_path;
+    public String category;
+    public String backdrop_path;
+    public String language;
+    public String country;
+    //public List<String> urlsToLogos;
+    public List<String> sortBysAvailable;
+    //public int color;
+
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl431/tugaspribadi/SourcesResponse.java
@@ -0,0 +1,9 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl431.tugaspribadi;
+
+import java.util.List;
+
+public class SourcesResponse {
+    public String page;
+    public List<Source> results;
+
+}
